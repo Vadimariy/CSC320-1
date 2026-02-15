c27f0d24d9ccf0f1daf9918656e39f897672a04f
joshg <joshgibs66@gmail.com>
2026-02-15 14:17:11 -0500

    New commit

---
+++ PersonalInfo.java
@@ -0,0 +1,16 @@
+public class PersonalInfo {
+    public static void main(String[] args) {
+
+        String firstName = "John";
+        String lastName = "Doe";
+        String streetAddress = "123 Main Street";
+        String city = "Denver";
+        String zipCode = "80202";
+
+        System.out.println(firstName);
+        System.out.println(lastName);
+        System.out.println(streetAddress);
+        System.out.println(city);
+        System.out.println(zipCode);
+    }
+}
