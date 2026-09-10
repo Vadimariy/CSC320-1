5a39a71ae6520f71aa9560c46e21112b7405fd92
JoshG <vadimariy@gmail.com>
2026-09-10 09:59:11 -0400

    new

--- Practice/example.java
+++ Practice/example.java
@@ -1,1 +1,16 @@
-import java.util.Scanner; public class WhileLoopExample { public static void main(String[] args) { Scanner input = new Scanner(System.in); String password = ""; while (!password.equals("Java123")) { System.out.print("Enter your password: "); password = input.nextLine(); } System.out.println("Correct password! Access granted."); input.close(); } }
+import java.util.Scanner;
+
+public class WhileLoopExample {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String password = "";
+
+        while (!password.equals("Java123")) {
+            System.out.print("Enter your password: ");
+            password = input.nextLine();
+        }
+
+        System.out.println("Correct password! Access granted.");
+        input.close();
+    }
+}
